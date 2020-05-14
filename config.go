587c11cf7567be97339f675a9e1a35f3574e11a1
Administrator <admin@example.com>
2020-05-14 13:58:15 +0100

    added support for env vars

--- config.go
+++ config.go
@@ -4,29 +4,32 @@ import (
 	"io/ioutil"
 	"time"
 
+	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/go-yaml/yaml.v2"
 )
 
 // Config struct
 type Config struct {
+	Loglevel string `yaml:"loglevel" envconfig:"URL_CHECKER_LOGLEVEL"`
+
 	Server struct {
-		Port int
-	}
+		Port int `yaml:"port" envconfig:"URL_CHECKER_SERVER_PORT"`
+	} `yaml:"server"`
 
 	Client struct {
-		SkipSSL bool
-		Timeout time.Duration
-		Period  time.Duration
-	}
+		SkipSSL bool          `yaml:"skipssl"`
+		Timeout time.Duration `yaml:"timeout" envconfig:"URL_CHECKER_TIMEOUT"`
+		Period  time.Duration `yaml:"period" envconfig:"URL_CHECKER_PERIOD"`
+	} `yaml:"client"`
 
 	Urls []string
 }
 
-var config Config
+var config Config // globally accessible
 
 func loadConfiguration(filename string) {
-	log.Info("INFO: Loading configuration...")
+	log.Info("Loading configuration...")
 	source, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Error("An error occured: ", err)
@@ -39,4 +42,11 @@ func loadConfiguration(filename string) {
 	}
 	log.Info("Config has been sucessfully loaded.")
 	log.Debug("Config map: ", config)
+}
+
+func readEnv(cfg *Config) {
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		log.Error("And error occured in readEnvVars(): Error message: \n\t", err)
+	}
 }
