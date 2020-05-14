422c86721e510a2b82c9f992de964e567d717cb5
Administrator <admin@example.com>
2020-05-14 08:49:07 +0100

    server code cleanup

--- server.go
+++ server.go
@@ -5,39 +5,17 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
-// This handler does something a little more sophisticated
-// by reading all the HTTP request headers and echoing
-// them into the response body.
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
 func metrics(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "# HELP Version: 0.1 Alpha - https://github.com/arturgut/urlchecker\n")
+	fmt.Fprintf(w, "# HELP url_checker. Label: HTTP Response code. Value: Request duration in Ms\n")
 	for key, value := range u {
-		// Prometheus format:
-		// <metric name>{<label name>=<label value>, ...}
+		// Prometheus format: <metric name>{<label name>=<label value>, ...}
 		// http_requests_total{service="service", server="pod50", env="production"}
-		fmt.Fprintf(w, "url_checker{ %v: http_status_code=%v, request_duration=%v}\n", key, value.responseCode, value.durationInMs)
+		fmt.Fprintf(w, "url_checker{ url='%v', http_status_code=%v } %v\n", key, value.responseCode, value.durationInMs)
 	}
-}
-
-func version(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Version: 0.1 ")
 }
 
 func startServer(port int) {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/metrics", metrics)
-	http.HandleFunc("/version", version)
 	http.ListenAndServe(":8090", nil)
 }
