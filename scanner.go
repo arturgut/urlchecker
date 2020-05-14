8959097d1912e22cc78ad5f42c9ca8f66cd070d0
Administrator <admin@example.com>
2020-05-14 08:49:39 +0100

    changed elapsed type to int64, code cleanup

--- scanner.go
+++ scanner.go
@@ -13,15 +13,14 @@ func urlScan(url string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	t := time.Now()
-	elapsed := t.Sub(start)
+	elapsed := int64(t.Sub(start)) / 1000 / 1000 // Convert to miliseconds
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 	fmt.Println("DEBUG: urlScan() -> resp result:", resp)
-	fmt.Println("DEBUG:Duration in ms", elapsed, "Duration as string() is:", string(elapsed))
+	fmt.Println("DEBUG:Duration in ms", elapsed, "Duration as string() is:", elapsed)
 	// Set map value
 	u[url] = ScanResult{resp.StatusCode, elapsed}
-	fmt.Println("DEBUG: HTTP RESPONSE CODE:", u[url].responseCode, u[url].durationInMs)
-	fmt.Println("DEBUG: elapsed is type of:", reflect.TypeOf(elapsed))
+	fmt.Println("DEBUG: elapsed ", elapsed, "which is typeOf", reflect.TypeOf(elapsed))
 }
