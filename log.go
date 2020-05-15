a717d9ee8fdb95b0f7f75bd2ddcab79ca7ac5142
Administrator <admin@example.com>
2020-05-15 11:25:52 +0100

    added configuration to log, updated config

--- log.go
+++ log.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
 func initLogging() {
 
 	log.SetLevel(log.DebugLevel) // Only log the warning severity or above.
-	log.Info("Starting URL checker. Version: 0.1")
 
+	formatter := &logrus.TextFormatter{
+		TimestampFormat:        "02-01-2006 15:04:05", // the "time" field configuratiom
+		FullTimestamp:          true,
+		DisableLevelTruncation: true, // log level field configuration
+	}
+	logrus.SetFormatter(formatter)
+
+	log.Info("Starting URL checker. Version: 0.1")
 }
